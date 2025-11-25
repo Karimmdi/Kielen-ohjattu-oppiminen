984fd14dcd8a4c36221a2f87b676a7322ed33f81
Karim Ahmadi <karim.ahmadi@tuni.fi>
2025-11-25 13:21:20 +0200

    edited:conf-settings, ai-service

--- config/settings.py
+++ config/settings.py
@@ -10,13 +10,17 @@ class Config:
   CARD_FLIP_DELAY: int = 3000
   DATA_DIR: str = "languages"
   IMAGES_DIR: str = "images"
-  
+
   # AI Configuration
   GEMINI_API_KEY: str = os.getenv("GEMINI_API_KEY", "")
   GEMINI_MODEL: str = "gemini-2.5-flash"
   PRELOAD_MIN_SENTENCES: int = 3
   SENTENCE_INTERVAL: int = 5
   WORD_BATCH_SIZE: int = 10
-  SYSTEM_INSCTRUCTIONS="You are a sentence generator and a translator. Your job is to generate accurate sentences that has both Finnish and English equivalents."
-
+  SYSTEM_INSTRUCTIONS: str ="You are a bilingual sentence generator and translator. Your task is to generate natural, " \
+  "grammatically correct sentences in both Finnish and English. " \
+  "Each sentence must:Follow the standard grammar and syntax rules of both languages." \
+  "Use appropriate verb forms, tenses, and word order." \
+  "Be contextually relevant and culturally appropriate." \
+  "Maintain the same meaning, tone, and nuance in both languages."
 config = Config()
